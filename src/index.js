f365b7b4fc91bc51c9df9b5407074f1fdb6e37a9
Emmanuel <oyedelee22@gmail.com>
2022-11-05 13:15:06 +0000

    hashroute removed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ReactDom from 'react-dom' 
-import { HashRouter } from 'react-router-dom'
+
 import App from './App' 
 import './main.css'
 
 ReactDom.render(
-    <HashRouter>
-        <App />
-        </HashRouter>, document.getElementById("root"))
+    
+        <App />,
+         document.getElementById("root"))
